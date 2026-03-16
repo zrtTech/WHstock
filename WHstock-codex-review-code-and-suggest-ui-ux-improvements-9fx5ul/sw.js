1c89cab81902a5a0f44c02cf3113787d7a029e02
zrtTech <162508530+zrtTech@users.noreply.github.com>
2026-03-16 20:32:41 +0300

    Add files via upload

---
+++ WHstock-codex-review-code-and-suggest-ui-ux-improvements-9fx5ul/sw.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = 'whstock-v1';
+const ASSETS = ['./', './index.html', './manifest.webmanifest'];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k))))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(
+    caches.match(event.request).then((cached) =>
+      cached || fetch(event.request).then((resp) => {
+        const copy = resp.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy)).catch(() => {});
+        return resp;
+      }).catch(() => caches.match('./index.html'))
+    )
+  );
+});
